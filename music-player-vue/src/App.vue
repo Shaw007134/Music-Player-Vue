c0e648f6482c8c7ae3635b52bc19ee89046858e7
shaw <gggshaw007@gmail.com>
2019-01-12 12:38:21 +0800

    添加首页Tab切换以及Recommend组件编写

---
+++ music-player-vue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <!-- 标签可以小写 -->
+    <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  //class的书写规范是第一个要大写
+  import MHeader from 'components/m-header/m-header';
+  import Tab from 'components/tab/tab';
+  export default {
+    components: {
+      MHeader,
+      Tab
+    }
+  };
+</script>
+
+<style scope lang="scss">
+
+</style>
---
+++ music-player-vue/src/main.js
@@ -0,0 +1,17 @@
+import 'commons/style/index.scss';
+import 'babel-polyfill';
+import fastclick from 'fastclick';
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+
+fastclick.attach(document.body);
+// Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  // router,
+  render: h => h(App),
+  router,
+});
