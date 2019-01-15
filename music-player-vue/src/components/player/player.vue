aa202ae40ff5a8e6e508cafb2f0cf841c16e9f5a
shaw <gggshaw007@gmail.com>
2019-01-15 17:26:41 +0800

    完成进度条功能以及拖动响应

--- music-player-vue/src/components/player/player.vue
+++ music-player-vue/src/components/player/player.vue
@@ -30,7 +30,7 @@
           <div class="progress-wrapper">
             <span class="time time-l">{{format(currentTime)}}</span>
             <div class="progress-bar-wrapper">
-              <progress-bar :percent="percent"></progress-bar>
+              <progress-bar :percent="percent" @percentChange="onProgressBarChange"></progress-bar>
             </div>
             <div class="time time-r">{{format(currentSong.duration)}}</div>
           </div>
@@ -211,6 +211,12 @@ export default {
       const minute = this._pad(interval / 60 | 0)
       const second = this._pad(interval % 60)
       return `${minute}:${second}`
+    },
+    onProgressBarChange(percent) {
+      this.$refs.audio.currentTime = this.currentSong.duration * percent
+      if(!this.playing) {
+        this.togglePlaying()
+      }
     },
     _pad(num, n=2) {
       let len = num.toString().length
