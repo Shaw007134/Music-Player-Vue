39f735c932c9a6dda9b6dc9ff7fa381a9c3ff0de
shaw <gggshaw007@gmail.com>
2019-01-18 17:11:14 +0800

    添加搜索历史列表组件

---
+++ music-player-vue/src/base/search-list/search-list.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="search-list" v-show="searches.length">
+    <transition-group name="slide" tag="ul">
+      <li class="search-item" v-for="item in searches" :key="item">
+        <span class="text">{{item}}</span>
+        <span class="icon">
+          <i class="icon-delete"></i>
+        </span>
+      </li>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    searches: {
+      type: Array,
+      default: []
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "commons/style/variable.scss";
+@import "commons/style/mixin.scss";
+
+.search-item {
+  display: flex;
+  align-items: center;
+  height: 40px;
+  overflow: hidden;
+  color: $color-text-l;
+  &.slide-enter,
+  &.slide-leave-to {
+    height: 0;
+  }
+  &.slide-enter-active,
+  &.slide-leave-active {
+    transition: all 0.1s;
+  }
+  .text {
+    flex: 1;
+    font-size: $font-size-medium-x;
+    @include ellipsis();
+  }
+  .icon {
+    @include extend-click();
+    .icon-close {
+      font-size: $font-size-small;
+      color: $color-text-d;
+    }
+  }
+}
+</style>
